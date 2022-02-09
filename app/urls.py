0c9382430754bf7efef927ea1ba6a83a12fa545b
kennedybabu <kennedynjogu8@gmail.com>
2022-02-09 13:04:35 +0300

    feature: create a landingpage

--- app/urls.py
+++ app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 
 urlpatterns=[
-    path("", views.home, name='home')
+    path("", views.landingpage, name='landingpage'),
+    path("home/", views.home, name='home')
 ]
--- app/views.py
+++ app/views.py
@@ -3,5 +3,8 @@ from django.shortcuts import render
 # Create your views here.
 
 
+def landingpage(request):
+    return render(request, 'app/landingpage.html')
+
 def home(request):
     return render(request, 'app/home.html')
