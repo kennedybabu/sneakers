b2038a6be98cf338ee25dbc1c9ab4ea394586990
kennedybabu <kennedynjogu8@gmail.com>
2022-02-10 14:21:22 +0300

    update the model

--- app/models.py
+++ app/models.py
@@ -4,10 +4,16 @@ from django.db import models
 
 
 class Shoe(models.Model):
+    choices = (
+        ('M', 'male'),
+        ('F', 'female'),
+        ('Uni', 'unisex')
+    )
+    
     name = models.CharField(max_length=200)
     description = models.TextField(null=True, blank=True)
-    # price = models.IntegerField()
-    # gender = models.IntegerChoices()
+    price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    gender = models.CharField(max_length=10, choices=choices, null=True)
     updated = models.DateTimeField(auto_now=True)
     date_posted = models.DateTimeField(auto_now_add=True)
 
