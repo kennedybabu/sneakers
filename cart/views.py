d4bab2fa2e3aea54386b06bf78e37050ce417afb
kennedybabu <kennedynjogu8@gmail.com>
2022-02-13 19:00:04 +0300

    feature: contextualise the cart to make it available throughout the application

--- cart/views.py
+++ cart/views.py
@@ -14,7 +14,7 @@ def cart_add(request, product_id):
     if form.is_valid():
         cd = form.cleaned_data
         cart.add(product=product, quantity=cd['quantity'], override_quantity=cd['override'])
-    return redirect('cart:cart_detail')
+    return redirect('home')
 
 
 @require_POST
