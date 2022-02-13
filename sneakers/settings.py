9ffc16f6d7e66f2e43fe44935d87e893acb45120
kennedybabu <kennedynjogu8@gmail.com>
2022-02-13 13:35:44 +0300

    feature: create cart app

--- sneakers/settings.py
+++ sneakers/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     'app',
+    'cart',
     'bootstrap4',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -40,6 +41,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 ]
+
+CART_SESSION_ID = 'cart'
 
 AUTH_USER_MODEL='app.User' 
 
