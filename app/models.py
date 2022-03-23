e598fdd2312281fd37bfcada2998804aea443d3e
kennedybabu <kennedynjogu8@gmail.com>
2022-03-23 08:57:12 +0300

    update models to use cloudinary storage

--- app/models.py
+++ app/models.py
@@ -13,7 +13,7 @@ class User(AbstractUser):
     email = models.EmailField(null=True, unique=True)
     bio = models.TextField(null=True)
 
-    avatar = models.ImageField(null=True, upload_to='images/')
+    avatar = CloudinaryField('image', null=True)
 
     REQUIRED_FIELDS = []
 
@@ -45,7 +45,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2, null=True)
     category = models.ForeignKey(Category, related_name='products', on_delete=models.CASCADE)
     updated = models.DateTimeField(auto_now=True)
-    image = models.ImageField(blank=True, upload_to='images/')
+    image = CloudinaryField('image',blank=True)
     created = models.DateTimeField(auto_now_add=True)
     slug = models.SlugField(max_length=200, db_index=True)
 
--- sneakers/settings.py
+++ sneakers/settings.py
@@ -158,7 +158,7 @@ STATICFILES_DIRS = [
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
-MEDIA_ROOT = BASE_DIR / 'static/images'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
